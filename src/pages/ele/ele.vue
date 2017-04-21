3a527afbaf98fbefa049119545c094f2331924fc
taylor <376977792@qq.com>
2017-04-21 16:26:35 +0800

    add element-theme

--- src/pages/ele/ele.vue
+++ src/pages/ele/ele.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <el-button @click="visible = true">按钮</el-button>
+    
   </div>
 </template>
 
 <script>
   export default {
     data: function () {
-      return { visible: false }
+      return {
+      }
+    },
+    computed: {
+    },
+    methods: {
     }
   }
 
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
-	@import "./style.scss";
+  @import "./style.scss";
 </style>
--- src/pages/ele/index.js
+++ src/pages/ele/index.js
@@ -1,11 +1,28 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import ElementUI from 'element-ui';
 import App from './ele.vue';
 
-Vue.use(ElementUI)
-new Vue({
-  el: '#app',
-  components: {
-    'app': App
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+
+const User = {
+  template: '<div>User {{$route.params.id}}</div>',
+  watch: {
+     $route(to, from) {
+     }
   }
+}
+
+const routes = [
+  { path: '/user/:id', component: User }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+new Vue({
+  router
+  // render: h => h(App)
+}).$mount('#app')
