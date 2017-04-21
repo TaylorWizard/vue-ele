c069f124381d66be924aaededbfe5f36009619cc
taylor <376977792@qq.com>
2017-04-21 18:45:06 +0800

    element

--- src/pages/ele/ele.vue
+++ src/pages/ele/ele.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    
+    <el-button @click="counter()">counter</el-button>
+    <el-progress type="circle" :percentage="data.percent"></el-progress>
+    {{data.percent}}
   </div>
 </template>
 
@@ -8,11 +10,28 @@
   export default {
     data: function () {
       return {
+        visible: false,
+        data: { percent: 20 },
+        message: "123456789"
       }
     },
     computed: {
+      status: function () {
+        let status = {
+          time: 500
+        }
+        return status;
+      }
     },
     methods: {
+      counter: function () {
+        let timer = setTimeout(() => {
+          if (this.data.percent === 50) return;
+          clearTimeout(timer);
+          this.counter();
+          this.data.percent++;
+        }, 1000);
+      }
     }
   }
 
--- src/pages/ele/index.js
+++ src/pages/ele/index.js
@@ -2,27 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ElementUI from 'element-ui';
 import App from './ele.vue';
+import {nav,router} from './components/nav.vue';
+
 
 Vue.use(ElementUI);
 Vue.use(VueRouter);
 
-const User = {
-  template: '<div>User {{$route.params.id}}</div>',
-  watch: {
-     $route(to, from) {
-     }
-  }
-}
-
-const routes = [
-  { path: '/user/:id', component: User }
-]
-
-const router = new VueRouter({
-  routes
-})
-
 new Vue({
-  router
-  // render: h => h(App)
-}).$mount('#app')
+  el: "#app",
+  router,
+  methods: {
+  }
+})
